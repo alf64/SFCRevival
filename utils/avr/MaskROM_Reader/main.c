3bc33f04a0c631159094f6897a41fef2b0944e63
alf64 <alf64gordon@gmail.com>
2018-02-12 21:09:18 +0100

    Added first avr/util - MaskROM_Reader. Initial setup.

---
+++ utils/avr/MaskROM_Reader/main.c
@@ -0,0 +1,17 @@
+/*
+ * main.c
+ *
+ *  Created on: 12 lut 2018
+ *      Author: alf64
+ */
+
+#include <stdio.h>
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+    for(;;);
+
+    return 0;
+}
